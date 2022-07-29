931133aa115944cd14bbec2c48aad46a246e502c
Andre macedo machado <andremacedomachado@gmail.com>
2022-07-29 14:16:22 -0300

    feat: Adicionado tema escuro e troca de tema

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,16 @@
 import { Button } from "@mui/material"
 import { Navigate, Route, Routes } from "react-router-dom"
 
+
+import { useAppThemeContext } from '../shared/contexts'
 export const AppRoutes = () => {
+    const {toggleTheme} = useAppThemeContext();
+
+
     return (
         <Routes>
-            <Route path='pagina-inicial' element={<Button variant="contained" color="primary">teste</Button>}></Route>
+            <Route path='pagina-inicial' element={<Button variant="contained" color="primary" onClick={toggleTheme}>teste</Button>}></Route>
+            <Route path='pagina' element={<p>teste</p>}></Route>
             <Route path='*' element={<Navigate to='pagina-inicial' />}></Route>
         </Routes>
     )
---
+++ src/shared/themes/Dark.ts
@@ -0,0 +1,27 @@
+import { createTheme } from '@mui/material'
+import { brown, orange } from '@mui/material/colors'
+
+export const DarkTheme = createTheme(
+    {
+
+        palette: {
+            mode: 'dark',
+            primary: {
+                main: orange[700],
+                dark: orange[800],
+                light: orange[500],
+                contrastText: '#ffffff',
+            },
+            secondary: {
+                main: brown[700],
+                dark: brown[800],
+                light: brown[500],
+                contrastText: '#ffffff',
+            },
+            background: {
+                paper: '#303134',
+                default: '#202124',
+            }
+        }
+    }
+)
