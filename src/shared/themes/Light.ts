2a8cc01ec6b6719eeb6d6a5b6c6fbce6c8d990cf
Andre macedo machado <andremacedomachado@gmail.com>
2022-07-29 12:00:39 -0300

    feat: Adicionado tema claro e provider

---
+++ src/shared/themes/Light.ts
@@ -0,0 +1,25 @@
+import { createTheme } from '@mui/material'
+import { brown, orange } from '@mui/material/colors'
+
+export const LightTheme = createTheme(
+    {
+        palette: {
+            primary: {
+                main: orange[700],
+                dark: orange[800],
+                light: orange[500],
+                contrastText: '#ffffff',
+            },
+            secondary: {
+                main: brown[700],
+                dark: brown[800],
+                light: brown[500],
+                contrastText: '#ffffff',
+            },
+            background: {
+                paper: '#ffffff',
+                default: '#f7f6f3',
+            }
+        }
+    }
+)
