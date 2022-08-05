1a14a5129435ad56828988c604f557ceaf5b00a3
Andremacedomachado <andremacedomachado@gmail.com>
2022-08-05 12:13:57 -0300

    refactor: atualizado page pessoas com urlParams filter

--- src/pages/pessoas/Pessoas.tsx
+++ src/pages/pessoas/Pessoas.tsx
@@ -1,9 +1,25 @@
+import { useMemo } from 'react';
+import { useSearchParams } from 'react-router-dom';
 import { FerramentaDeListagem } from '../../shared/components';
 import { LayoutBaseDePagina } from '../../shared/layouts';
 
 
 export const Pessoas = () => {
+    const [searchParams, setSearchParams] = useSearchParams();
+    const busca = useMemo(() => {
+        return searchParams.get('busca') || '';
+    }, [searchParams]);
     return (
-        <LayoutBaseDePagina titulo="Pessoas" barraDeFerramentas={< FerramentaDeListagem mostrarInputBusca />} />
+        <LayoutBaseDePagina
+            titulo="Pessoas"
+            barraDeFerramentas={
+                < FerramentaDeListagem
+                    mostrarInputBusca
+                    textoBotaoNovo='Nova'
+                    textoDeBusca={busca}
+                    aoMudarTextoDeBusca={texto => setSearchParams({ busca: texto }, { replace: true })}
+                />
+            }
+        />
     );
 };
